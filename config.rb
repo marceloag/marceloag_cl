e6a3bac596688e50cfaf7003443e0739a2d89cb7
Marcelo Aguila <web@croop.cl>
2017-02-27 12:04:55 -0300

    responsive y mail

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 # Require any additional compass plugins here.
+require 'breakpoint'
 add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
@@ -15,7 +16,7 @@ javascripts_dir = "js"
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
